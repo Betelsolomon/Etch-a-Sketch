c54404f874beead840c7f67b5be92eaf0e332bd6
beth <bebrave1214@gmail.com>
2024-11-17 02:13:30 +0300

    add createGrid function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const container= document.querySelector(".container");
 const resizeButton = document.querySelector("#resizeButton");
 
-
+function creatGrid(size){
+  
+    container.innerHTML = '';
+   
+    for (let i = 0; i < size * size; i++) {
+     const newDiv = document.createElement("div");
+     newDiv.classList.add("child");
+     container.appendChild(newDiv);
+ 
+    }
+    
+ }
+ creatGrid(16);
 
